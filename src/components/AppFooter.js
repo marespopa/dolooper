1c03bedb4357b54169bef70c5e7920ecc8c0ea66
Mares Popa <popa.mares@gmail.com>
2020-03-06 17:00:17 +0200

    Update footer
    - change footer color
    - update footer message

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -2,13 +2,14 @@ import React from "react";
 
 const AppFooter = () => (
   <footer className="app__footer">
+    created by Mares Popa
     <a
       className="suggestions-link"
       target="_blank"
       rel="noopener noreferrer"
       href="https://github.com/xkrromania/dailywork/issues"
     >
-      You can leave feedback here.
+      You can leave feedback here
     </a>
   </footer>
 );
