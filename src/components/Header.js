751f73fc527daec486025e2c3a54efd114e13823
Mares Popa <popa.mares@gmail.com>
2022-01-04 20:07:27 +0200

    - update styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import MainNavigation from './MainNavigation'
-import ReactLogo from '../assets/logo.svg'
+import AppLogo from '../assets/logo.svg'
 
 const Header = () => (
   <header className="app__header">
     <section className="content">
       <h1 className="app__title">
         <NavLink to="/">
-          <img src={ReactLogo} alt="DevWork"></img>
+          <img src={AppLogo} alt="DevWork"></img>
         </NavLink>
       </h1>
       <MainNavigation />
