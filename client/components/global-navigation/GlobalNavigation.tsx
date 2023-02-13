101991eb031da7870941065c8e4f85aaef65525a
Mares Popa <popa.mares@gmail.com>
2023-02-13 17:43:56 +0200

    Add Twitter intent

--- client/components/global-navigation/GlobalNavigation.tsx
+++ client/components/global-navigation/GlobalNavigation.tsx
@@ -3,9 +3,12 @@ import Link from 'next/link'
 import Container from '../container/Container.component'
 import logo from '../../public/logo.svg'
 import Image from 'next/image'
+import TwitterSVG from '../../icons/TwitterSVG'
 
 const GlobalNavigation = () => {
   const logoHeight = 24
+  const twitterLink = `https://twitter.com/intent/tweet?text=Get%20productive%20with%20doloper!%20%20&url=
+                      https%3A%2F%2Fdoloper.netlify.com%2F`
 
   return (
     <header className="w-full text-gray-800 bg-amber-100 sticky top-0 z-10">
@@ -18,6 +21,11 @@ const GlobalNavigation = () => {
                   <Image src={logo} alt="Doloper" height={logoHeight} />
                 </Link>
               </div>
+            </div>
+            <div>
+              <a href={twitterLink} target="_blank" rel="noreferrer">
+                <TwitterSVG />
+              </a>
             </div>
           </div>
         </Container>
