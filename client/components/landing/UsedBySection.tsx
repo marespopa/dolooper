f30d80ccf3d80a1a3b35b4fecc95892ea0ea4097
Mares Popa <popa.mares@gmail.com>
2023-03-29 11:15:44 +0300

    housekeeping: Relative imports - update relative imports - fix npm problems

--- client/components/landing/UsedBySection.tsx
+++ client/components/landing/UsedBySection.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
 import React from 'react'
+import Image from 'next/image'
 
 const myLoader = () => {
   return `https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=377056&theme=dark`
@@ -9,8 +9,8 @@ const ProductHuntImage = () => {
   return (
     <Image
       loader={myLoader}
-      src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=377056&theme=dark"
-      alt="Doloper - Plan&#0046;&#0032;Focus&#0046;&#0032;Execute&#0046; | Product Hunt"
+      src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=386422&theme=neutral"
+      alt="Dolooper - Do&#0032;one&#0032;thing&#0032;at&#0032;a&#0032;time&#0044;&#0032;do&#0032;it&#0032;well&#0032;with&#0032;Dolooper&#0046; | Product Hunt"
       width={250}
       height={54}
     />
@@ -22,7 +22,7 @@ const UsedBySection = () => {
     <section className={sectionStyles}>
       <div className={containerStyles}>
         <a
-          href="https://www.producthunt.com/posts/doloper?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-doloper"
+          href="https://www.producthunt.com/posts/dolooper?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-dolooper"
           target="_blank"
           rel="noreferrer"
         >
