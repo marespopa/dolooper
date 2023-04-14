c082bd4a55d8ba6ceb26d5bb220ed2381758e7d0
Mares Popa <129362936+maresGoIT@users.noreply.github.com>
2023-04-14 11:52:27 +0300

    feat(overview): add border for sections

--- client/components/common/common.ts
+++ client/components/common/common.ts
@@ -1,2 +1,2 @@
-export const boxStyles = `bg-white rounded-lg shadow-sm`
+export const boxStyles = `bg-white rounded-lg shadow-sm border border-gray-200`
 export const pagePadding = `px-4 sm:px-2 lg:px-0`
