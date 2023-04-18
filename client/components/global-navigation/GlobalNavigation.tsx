0c9a11ab3ffc892372a29da1f3ebb45feb9715cc
Mares Popa <popa.mares@gmail.com>
2023-04-18 17:30:20 +0300

    feat(dark-mode): use system theme

--- client/components/global-navigation/GlobalNavigation.tsx
+++ client/components/global-navigation/GlobalNavigation.tsx
@@ -1,18 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import Link from 'next/link'
 import Container from '../container/Container.component'
-import logoLight from '../../public/logo-light.svg'
-import logoDark from '../../public/logo-dark.svg'
 
-import Image from 'next/image'
 import ThemeToggle from './ThemeToggle'
-import { useTheme } from 'next-themes'
 import TwitterButton from './TwitterButton'
+import GlobalLogo from './GlobalLogo'
 
 const GlobalNavigation = () => {
-  const logoHeight = 32
-  const { theme } = useTheme()
-  const logoImg = theme === 'dark' ? logoDark : logoLight
   const [mounted, setMounted] = useState(false)
 
   useEffect(() => {
@@ -29,11 +22,7 @@ const GlobalNavigation = () => {
         <Container>
           <div className="justify-between mx-auto lg:max-w-7xl md:items-center md:flex">
             <div>
-              <div className="flex items-center justify-center lg:justify-between py-3 md:py-5 md:block">
-                <Link href="/">
-                  <Image src={logoImg} alt="Dolooper" height={logoHeight} />
-                </Link>
-              </div>
+              <GlobalLogo />
             </div>
             <div className="flex justify-center lg:justify-between py-3 md:py-5">
               <TwitterButton />
