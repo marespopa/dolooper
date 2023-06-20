a76b5b59476f97e2660f0bb048ab60f846d963ac
smoothwizz <popa.mares@gmail.com>
2023-06-20 15:49:51 +0300

    feat(editor): fix grid on mobile

--- client/components/planning/issue/Issue.component.tsx
+++ client/components/planning/issue/Issue.component.tsx
@@ -12,7 +12,7 @@ const Issue = (props: Props) => {
   const { value, handleUpdateValue } = props
 
   return (
-    <div className="grid grid-cols-2 gap-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
       {renderEditField()}
       {renderDisplayField()}
     </div>
