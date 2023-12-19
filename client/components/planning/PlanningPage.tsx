ffb4ffb113de7809df252d8c09fe7333061cc854
smoothwizz <popa.mares@gmail.com>
2023-12-19 17:15:35 +0200

    feat: remove description restriction

--- client/components/planning/PlanningPage.tsx
+++ client/components/planning/PlanningPage.tsx
@@ -1,30 +1,17 @@
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
-import StorageService from '../../services/storageService'
+import React from 'react'
 import Container from '../container'
 import IssueSection from './issue/IssueSection.component'
 import TasksSection from './tasks/TasksSection.component'
 import { pagePadding } from '../common/common'
-import Alert from '../banners/Alert'
 import Greeting from './greeting/Greeting.component'
 import ButtonPrimary from '../forms/buttons/ButtonPrimary'
 
 const PlanningPage = () => {
   const router = useRouter()
-  const [error, setError] = useState('')
-  const hasError = error.length > 0
 
   const handleCompletePlanning = async () => {
-    const hasDescription = await StorageService.hasDescription()
-
-    if (hasDescription) {
-      setError('')
-      router.push('/overview')
-
-      return
-    }
-
-    setError('You need to define your task before starting the work session.')
+    router.push('/overview')
   }
 
   return (
@@ -42,11 +29,6 @@ const PlanningPage = () => {
             text={'Complete Planning'}
           />
         </div>
-        {hasError && (
-          <div className="-mt-8 mb-2">
-            <Alert style="error">{error}</Alert>
-          </div>
-        )}
       </Container>
     </div>
   )
